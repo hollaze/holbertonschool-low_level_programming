e33b7dec0854f4966834613bb5c7fab444724c25
hollaze <deperiers.a@gmail.com>
2021-02-03 18:30:16 +0000

    0x00-hello_world/

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
 *main
 *print                                                                                                                  *printf
 *return
-*/
+0*/
 
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("\"with proper grammar, but the outcome is a piece of art");
 	return(0);
 }
