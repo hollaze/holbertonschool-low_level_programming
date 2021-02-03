460d0162647947d4431a0c1d16245bcf61e09f72
hollaze <deperiers.a@gmail.com>
2021-02-03 18:18:26 +0000

    testing all my new files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\" Size of a char : %zu byte(s)", sizeof(char));
+	printf("\" Size of an int: %zu byte(s)", sizeof(int));
+	printf("\" Size of a long int : %zu byte(s)", sizeof(long int));
+	printf("\" Size of a long long int : %zu byte(s)", sizeof(long long int));
+	printf("\" Size of a float : %zu byte(s)", sizeof(float));
+	
+	return(0)
+}
+
+/*
+*main
+*Print char
+*Print int
+*Print long int
+*Print long long int
+*float
+*Return(0)
+*/
