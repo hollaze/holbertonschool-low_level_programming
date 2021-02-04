e8b9268b0e48b292601e097a13abdf195818ea5d
hollaze <deperiers.a@gmail.com>
2021-02-04 05:38:58 +0000

    0x00-hello_world/

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /*
 *main
 *print                                                                                                                  *printf
-*return
-0*/
+*return0
+*/
 
 int main(void)
 {
 	printf("\"with proper grammar, but the outcome is a piece of art");
-	return(0);
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,5 @@ int main(void)
 	printf("\" Size of a long int : %zu byte(s)", sizeof(long int));
 	printf("\" Size of a long long int : %zu byte(s)", sizeof(long long int));
 	printf("\" Size of a float : %zu byte(s)", sizeof(float));
-	
-	return(0)
+	return (0);
 }
