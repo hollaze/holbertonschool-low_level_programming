f9f73ebfce7d85c45b87cfc3b311a5bce6a519e2
hollaze <deperiers.a@gmail.com>
2021-02-04 11:19:33 +0000

    0x01-variables_if_else_while/

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, print alphabet without q and e
+ *
+ * initialization of char alphabet
+ * for loop for pritinf alphabet
+ * if condition for not printing q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	if (alphabet == 'q' || alphabet == 'e')
+	{
+	continue;
+	}
+
+	putchar(alphabet);
+}
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, commentaire
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+
+
+return (0);
+}
