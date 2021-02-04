dade832692cf4ee4942cf8de7aeb42f4ec91e5bb
hollaze <deperiers.a@gmail.com>
2021-02-04 14:51:52 +0000

    0x01-variables_if_else_while/

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print 0 to 10 without using char
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int base = 0;
+
+while (base < 10)
+putchar (base + '0');
+
+putchar ('\n');
+
+return (0);
+}
