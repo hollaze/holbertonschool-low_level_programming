dec0502b04234c6a22f82d0c1e5f6fd41f806ad2
hollaze <deperiers.a@gmail.com>
2021-02-05 09:03:51 +0000

    Fixing file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - commentaire
+ * main - print text with standart error 1
  *
  *
  * Return: Always 0 (Success)
@@ -10,7 +11,7 @@
 int main(void)
 {
 
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 }
