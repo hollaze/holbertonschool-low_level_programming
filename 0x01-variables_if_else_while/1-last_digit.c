3a060f01ae03b47ad0198bad0968b35adb835fbe
hollaze <deperiers.a@gmail.com>
2021-02-05 09:36:52 +0000

    Fixing file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -33,7 +33,7 @@ else if (l == 0)
 printf("Last digit of %d is %d and is 0", n, l);
 }
 
-else if (l < 6)
+else if (l < 6 && l != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0", n, l);
 }
