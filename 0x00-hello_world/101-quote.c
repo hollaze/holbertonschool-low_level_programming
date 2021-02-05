fd8968abe0a61374496e34eb317906aa4d426524
hollaze <deperiers.a@gmail.com>
2021-02-05 07:14:09 +0000

    0x00-hello_world/

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - commentaire
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (1);
+}
