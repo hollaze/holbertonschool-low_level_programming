d789f1faea40b5fd2a4ad68709753cfe96b0866b
hollaze <deperiers.a@gmail.com>
2021-02-08 02:27:40 +0000

    Adding file 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - checking absolute numbers
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+
+int _abs(int j);
+
+return (0);
+}
+
+/**
+ * _abs - print absolute value of an integer
+ * @j: the number to be check
+ *
+ * Return: abs, the absolute value of the integer j
+ */
+
+int _abs(int j)
+{
+
+return (abs(j));
+
+}
