d0d6a76350ec3a042c62b23af2c95d674a4c7af5
hollaze <deperiers.a@gmail.com>
2021-02-08 02:41:44 +0000

    Adding file 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - return int function
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int print_last_digit(int);
+
+return (0);
+}
+
+/**
+ * print_last_digit - function that print the last digit
+ * @a: int to be chek
+ *
+ * Return: last digit of int a
+ */
+
+int print_last_digit(int a)
+{
+a = a % a;
+
+return (a);
+
+}
