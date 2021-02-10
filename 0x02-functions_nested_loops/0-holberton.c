c19b5e30ffa645beadd6463402b631a59c91d482
hollaze <deperiers.a@gmail.com>
2021-02-10 09:01:00 +0000

    adding holberton.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -12,17 +12,17 @@
 int main(void)
 {
 
-putchar('H');
-putchar('o');
-putchar('l');
-putchar('b');
-putchar('e');
-putchar('r');
-putchar('t');
-putchar('o');
-putchar('n');
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 
-putchar('\n');
+_putchar('\n');
 
 return (0);
 }
