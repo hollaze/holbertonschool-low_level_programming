f20bad861a4152c2c3bd844531cb9ab5375d53a2
hollaze <deperiers.a@gmail.com>
2021-02-11 08:44:53 +0000

    Adding dir 0x04 + 3 first files

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *
+ * _isdigit - check if c is digit
+ * @c: the variable to be check
+ * Return: 1 if c is a digit. 0 otherwise
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	return (1);
+
+	else
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *
+ * mul - multiplies 2 integers
+ * @a: integer
+ * @b: integer
+ * Return: multiplication.
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
