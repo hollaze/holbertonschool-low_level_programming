36753b7f6e60b17d9da4a8a188ab5e19c9cf6d66
hollaze <deperiers.a@gmail.com>
2021-02-11 13:27:26 +0000

    0x04-more_functions_nested_loops/

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
 
 /**
- *
  * _isupper - function that check if c is in uppercase
  * @c: int to character
  * Return: 1 if c is uppercase. 0 otherwise
- *
  */
 
 int _isupper(int c)
