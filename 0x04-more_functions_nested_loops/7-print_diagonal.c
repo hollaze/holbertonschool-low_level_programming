087c7e9da127c1015348af0b21d92c040be5ce54
hollaze <deperiers.a@gmail.com>
2021-02-11 16:55:02 +0000

    Testing file 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draws diagonal line
+ * @n: lines
+ */
+
+void print_diagonal(int n)
+{
+int i, j;
+
+if (n > 0)
+{
+
+	for (i = 0; i < n; ++i)
+	{
+
+		for (j = 0; j < i; ++j)
+		{
+
+		_putchar(' ');
+
+		}
+
+	_putchar('\\');
+	_putchar('\n');
+	}
+
+} /* if n>0 */
+
+_putchar('\n');
+
+}
