a96fce7f6b14583c63644c0670a9bb6906a31ef1
hollaze <deperiers.a@gmail.com>
2021-02-11 13:41:37 +0000

    0x04-more_functions_nested_loops/

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,13 @@
 #include "holberton.h"
 
 /**
- *
  * mul - multiplies 2 integers
  * @a: integer
  * @b: integer
  * Return: multiplication.
- *
  */
 
 int mul(int a, int b)
 {
 	return (a * b);
-	_putchar('\n');
 }
