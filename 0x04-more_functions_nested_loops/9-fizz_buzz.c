378bd660f3ac15a72b154b1ea1c8ff38afd8f2d6
hollaze <deperiers.a@gmail.com>
2021-02-12 16:32:49 +0000

    0x04-more_functions_nested_loops/

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,23 +13,23 @@ int i;
 	for (i = 1; i <= 100; i++)
 	{
 
-	if (i % 3 == 0)
-	{
-	printf("Fizz");
-	putchar(' ');
-	}
+		if (i % 3 == 0)
+		{
+		printf("Fizz");
+		putchar(' ');
+		}
 
-	else if (i % 5 == 0 && i <= 100)
-	{
-	printf("Buzz");
-	putchar(' ');
-	}
+		else if (i % 5 == 0 && i <= 100)
+		{
+		printf("Buzz");
+		putchar(' ');
+		}
 
-	else
-	{
-	printf("%u", i);
-	putchar(' ');
-	}
+		else
+		{
+		printf("%u", i);
+		putchar(' ');
+		}
 
 	}
 
