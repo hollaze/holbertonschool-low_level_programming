425938272529f1cdae8bd0743133fc14ed9a3935
hollaze <deperiers.a@gmail.com>
2021-02-16 14:09:30 +0000

    0x05-pointers_arrays_strings/

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int i, length;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = (length - 1) / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
