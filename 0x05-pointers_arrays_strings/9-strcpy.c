c2a00782c16626c4e65e258311a8cc728943d472
hollaze <deperiers.a@gmail.com>
2021-02-17 16:20:30 +0000

    Adding file 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - Copies string src and paste in char dest
+ *
+ * @dest: char destination
+ * @src: char source
+ *
+ * Return: the pointer to dest&
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+return (dest);
+
+}
