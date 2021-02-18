a827a65fbb89732534be1b420ba488222574a9b1
hollaze <deperiers.a@gmail.com>
2021-02-18 18:17:34 +0000

    0x06-pointers_arrays_strings/

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,27 +4,26 @@
  * _strncat - concatenate two string with n character limitation
  * @dest: string destination
  * @src: string source
+ * @n: concatenation limitation
  *
  * Return: string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-
 	int index;
-        int length = 0;
+	int length = 0;
 
-        while (dest[length] != '\0')
-                length++;
+	while (dest[length] != '\0')
+		length++;
 
-        for (index = 0; src[index] != '\0'; index++, length++)
-        {
+	for (index = 0; src[index] != '\0'; index++, length++)
+	{
 		if (index < n)
-                dest[length] = src[index];
-        }
+			dest[length] = src[index];
+	}
 
-        dest[length] = '\0';
+	dest[length] = '\0';
 
-        return (dest);
-
+	return (dest);
 }
