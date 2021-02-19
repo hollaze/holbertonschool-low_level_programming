ff8cd4ab4911c99abd6efcc280810f612d6952ff
hollaze <deperiers.a@gmail.com>
2021-02-19 14:37:17 +0000

    times table are up

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,10 +6,42 @@
 
 void times_table(void)
 {
-int table0, table1, table2, table3, table4, table5, table6, table7, table8, table9;
 
-for (table0 = 0;;)
-}
-	
-	_putchar(table0);
+	int column, horizontal; /* Index */
+	int row = 10; /* Limit value */
+	int result;
+
+	for (column = 0; column < row; column++)
+	{
+		for (horizontal = 0; horizontal < row; horizontal++)
+		{
+			result = column * horizontal;
+
+			if (result <= 9)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+
+
+			else if (result > 9)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+
+
+			if (horizontal < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+
+		}
+
+		_putchar('\n');
+
+	}
+
 }
