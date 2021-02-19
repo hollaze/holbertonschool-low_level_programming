12047ef00d1544aeca093c18b783cc2469a1ab15
hollaze <deperiers.a@gmail.com>
2021-02-19 17:26:37 +0000

    up^to date

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,14 @@
 
 int print_last_digit(int a)
 {
+	int last_digit = a;
+	last_digit = last_digit % 10;
 
-return (a % 10);
+	if (last_digit < 0)
+		last_digit = last_digit * (- 1);
+
+	_putchar(last_digit + '0');
+
+return (last_digit);
 
 }
