62b38e9080001922a582328adc0bd9c1064d8660
hollaze <deperiers.a@gmail.com>
2021-02-21 16:40:47 +0000

    Fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,13 +8,15 @@
 void print_times_table(int n)
 {
 	int i, j, result;
+
 	for (i = 0; i <= n; i++)
 	{
 		if (n >= 0 && n <= 15)
 		{
-		for (j = 0; j <= n; j++)
-		{
+			for (j = 0; j <= n; j++)
+			{
 				result = i * j;
+
 				if (result <= 9)
 				{
 					if (j > 0)
@@ -41,7 +43,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 				}
-		}
+			}
 		_putchar('\n');
 		}
 	}
