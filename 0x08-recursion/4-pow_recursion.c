da480359c403314b60e8f4da6b46530878f5ae54
hollaze <deperiers.a@gmail.com>
2021-02-26 09:34:57 +0000

    testing file 5

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,17 +11,14 @@
 int _pow_recursion(int x, int y)
 {
 
-	if (x == 1 || y == 0)
+	if (y == 0)
 		return (1);
 
 	else if (y < 0)
 		return (-1);
 
-	else if (x > 0 || y > 0)
-	{
 		/* n * factorial(n - 1); */
-		return (x * _pow_recursion(x * y));
-	}
+		return (x * _pow_recursion(x, (y - 1)));
 
 
 }
