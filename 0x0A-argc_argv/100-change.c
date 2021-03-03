61d3c5a036e9d69fb44592423b2bf787ec722a58
hollaze <deperiers.a@gmail.com>
2021-03-03 21:38:24 +0000

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * main - coin change
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+{
+
+
+
+return (0);
+
+}
