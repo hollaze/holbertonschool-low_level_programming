5ca7d8588c31bde5392c56914b687ef39b29a69e
hollaze <deperiers.a@gmail.com>
2021-03-03 15:25:06 +0000

    5-argstostr.c

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits string into words
+ *
+ * @str: string
+ *
+ * Return: 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+
+	if (ac == 0 || av == NULL)
+                return (NULL);
+
+return (*av);
+
+}
