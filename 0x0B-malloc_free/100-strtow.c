2be9674cdf9ee7a704afc9e411a1c735556331cb
hollaze <deperiers.a@gmail.com>
2021-03-03 15:11:14 +0000

    test

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits string into words
+ *
+ * @str: string
+ *
+ * Return: 0
+ */
+
+char **strtow(char *str)
+{
+
+	if (str == NULL)
+		return (NULL);
+
+
+return (str);
+
+}
