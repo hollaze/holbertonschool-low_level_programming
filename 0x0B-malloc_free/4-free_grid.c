06972d7f98fa4587bc89198812de303df8be0972
hollaze <deperiers.a@gmail.com>
2021-03-03 21:55:39 +0000

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees 2 dimensional grid
+ *
+ * @grid: pointer to pointer
+ * @height: height of grid
+ */
+
+void free_grid(int **grid __attribute__((unused)), int height __attribute__((unused)))
+{
+
+}
