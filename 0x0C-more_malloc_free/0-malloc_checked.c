5c09c4a16910d7296b2191255b28899de31997f4
hollaze <deperiers.a@gmail.com>
2021-03-04 13:08:07 +0000

    test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ *
+ * @b: unsigned int
+ *
+ * Return: ptr
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		 exit(98);
+
+return (ptr);
+
+}
