373db1c99d2b4dd85f6a8e5e4b9385038f10b293
hollaze <deperiers.a@gmail.com>
2021-03-07 07:54:03 +0000

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <holberton.h>
+
+/**
+ * reverse_array - reverse array of integers
+ *
+ * @a: array of integers
+ * @n: number of elements in array
+ *
+ */
+
+void revers_array(int *a, int n)
+{
+
+	int temp, i, j;
+
+	for (i = (n - 1), j = 0; j < i; j++, i--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+}
