f6b8a644751d8862c8fda76ba4f1c4b4c6e6710f
hollaze <deperiers.a@gmail.com>
2021-03-09 17:24:19 +0000

    0x0E-structures_typedef/

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,49 @@
 #include "dog.h"
 
 /**
+ * _strlen - return length of string
+ * @s: string
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int i, length;
+
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+
+return (length);
+
+}
+
+/**
+ * _strcpy - Copies string src and paste in char dest
+ *
+ * @dest: char destination
+ * @src: char source
+ *
+ * Return: the pointer to dest&
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+return (dest);
+
+}
+
+/**
  * new_dog - create new dog struct
  *
  * @name: name member
@@ -23,18 +66,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (my_dog == NULL)
 		return (NULL);
 
-	cpy_name = malloc(sizeof(char) * strlen(name) + 1);
+	cpy_name = malloc(sizeof(char) * _strlen(name) + 1);
 
 	if (cpy_name == NULL)
 		return (NULL);
 
-	cpy_owner = malloc(sizeof(char) * strlen(owner) + 1);
+	cpy_owner = malloc(sizeof(char) * _strlen(owner) + 1);
 
 	if (cpy_owner == NULL)
 		return (NULL);
 
-	strcpy(cpy_name, name);
-	strcpy(cpy_owner, owner);
+	_strcpy(cpy_name, name);
+	_strcpy(cpy_owner, owner);
 
 	my_dog->name = name;
 	my_dog->age = age;
