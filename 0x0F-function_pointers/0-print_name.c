8a6afc587306bad3efab8ceb47e547e4ace6a9c1
hollaze <deperiers.a@gmail.com>
2021-03-10 16:59:57 +0000

    including stddef in library

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stddef.h>
 
 /**
  * print_name - print a name
@@ -11,7 +10,7 @@
 void print_name(char *name, void (*f)(char *))
 {
 
-	if (name == NULL || !f)
+	if (name == NULL || f == NULL)
 		return;
 
 	f(name);
