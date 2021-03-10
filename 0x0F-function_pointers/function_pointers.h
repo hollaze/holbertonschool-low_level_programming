c4964c34733531046f631da683df300e95b341a6
hollaze <deperiers.a@gmail.com>
2021-03-10 17:07:33 +0000

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,6 @@
-#ifndef FUNC_LIB
-#define FUNC_LIB
-
+#ifndef FUNC_PTR_LIB
+#define FUNC_PRT_LIB
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
 #endif
