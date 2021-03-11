9d35d472cfdb65b1c9ba6090ec763b4393b53292
hollaze <deperiers.a@gmail.com>
2021-03-11 20:49:54 +0000

    If n don't exist

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	int numbers;
 
-	if (separator == NULL || n == 0)
+	if (separator == NULL || !n)
 		return;
 
 	va_start(args, n);
