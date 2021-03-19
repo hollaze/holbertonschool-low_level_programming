d4619e04f5c725f2989eb054ca4eacb6cfe18fe9
hollaze <deperiers.a@gmail.com>
2021-03-19 15:14:39 +0000

    fixing betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
+void exe_before_main(void)__attribute__((constructor));
+
 /**
  * exe_before_main - execute function before main
- *
  */
-
-void exe_before_main(void)__attribute__((constructor));
 
 void exe_before_main(void)
 {
-	printf("%s", "You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("%s", "You're beat! and yet, you must allow,");
+	printf("%s", "\nI bore my house upon my back!\n");
 }
