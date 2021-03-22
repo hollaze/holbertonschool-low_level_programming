99104d05955108ca735e36112056784297359ab4
hollaze <deperiers.a@gmail.com>
2021-03-22 15:08:11 +0000

    need check

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum all n data
+ * (n is int in struct lists.h)
+ * @head: node head
+ * Return: sum of all n data
+ */
+
+int sum_listint(listint_t *head)
+{
+
+	int n = 0;
+
+	if (head == NULL)
+		return (n);
+
+	while (head != NULL)
+	{
+		n += head->n;
+		head = head->next;
+	}
+
+return (n);
+
+}
