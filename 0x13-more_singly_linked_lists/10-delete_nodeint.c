59cba5a97483ce683d754776e180312e0e2c13a4
hollaze <deperiers.a@gmail.com>
2021-03-23 17:28:11 +0000

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -37,7 +37,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (temp == NULL || temp->next == NULL)
 		return (-1);
 
-	new = temp->next;
+	new = temp->next->next;
 	free(temp);
 	temp->next = new;
 
