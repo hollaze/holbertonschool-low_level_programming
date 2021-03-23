2334d4bd1c81f49b202c00f043955ddfc099dedf
hollaze <deperiers.a@gmail.com>
2021-03-23 17:06:45 +0000

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 
-	while (temp->next != NULL && i < idx - 1)
+	while (temp != NULL && i < idx - 1)
 	{
 		temp = temp->next;
 		i++;
