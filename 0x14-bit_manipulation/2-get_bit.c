254914ca36b92448a0394b921bb7a61205404b07
hollaze <deperiers.a@gmail.com>
2021-03-26 10:18:43 +0000

    adding error condition

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int bitStatus;
 
+	/* machine got 64bits */
+	if (index > 64)
+		return (-1);
+
 	bitStatus = (n >> index) & 1;
 
 	n = bitStatus;
