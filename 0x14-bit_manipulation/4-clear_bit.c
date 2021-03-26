adc28e657192c9e3febcd41e46adeb4001092e75
hollaze <deperiers.a@gmail.com>
2021-03-26 11:06:10 +0000

    test

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * cleat_bit - clear bits
+ * @n: number
+ * @index: unsigned int
+ * Return: 1 if it worked, -1 if an error occurend
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > 64)
+		return (-1);
+
+	*n = *n & ~(1 << index);
+
+return (1);
+
+}
+
