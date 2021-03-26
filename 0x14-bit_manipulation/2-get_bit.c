9a72c9e6472aeec10b4ce8bf1e37435aa571fa66
hollaze <deperiers.a@gmail.com>
2021-03-26 10:01:45 +0000

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * get_bit - transform value into bits
+ * @n: number to convert
+ * @index: unsigned int
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int bitStatus;
+
+	bitStatus = (n >> index) & 1;
+
+	n = bitStatus;
+
+return (n);
+
+}
