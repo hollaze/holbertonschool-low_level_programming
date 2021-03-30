93967f05285d749b9cc05a7c782b0b22b9813154
hollaze <deperiers.a@gmail.com>
2021-03-30 19:29:37 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,20 +11,28 @@
 int create_file(const char *filename, char *text_content)
 {
 
-	int file;
-	char *buf;
+	int file, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
-	{
-		fd = open(filename, O_CREAT | O_RDWR);
-	}
+		text_content = "";
 
-	rd = read();
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	wr = write();
+	if (file == -1)
+		return (-1);
+
+	while (text_content && text_content[len])
+		len++;
+
+	wr = write(file, text_content, len);
+
+	close(file);
+
+	if (wr == -1)
+		return (-1);
 
 return (1);
 
