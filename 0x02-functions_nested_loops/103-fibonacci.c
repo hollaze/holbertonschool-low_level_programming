d8fd8785574fded62d428cfa8218cfc5508269a2
hollaze <deperiers.a@gmail.com>
2021-04-20 16:25:40 +0200

    checking

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 	long int fNum0 = 0, fNum1 = 1;
 	long int fSum = 0, long int limit = 4000000;
 
-	for (i = 0; fsum <= limit; i++)
+	for (i = 0; fSum <= limit; i++)
 	{
 		fSum = fNum0 + fNum1;
 		fNum0 = fNum1;
 		fNum1 = fSum;
 
-		if (fsum % 2 == 0)
+		if (fSum % 2 == 0)
 			printf("%li", fSum);
 
 		if (i < 49)
