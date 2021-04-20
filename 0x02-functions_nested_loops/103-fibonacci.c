caf693df9ae675a350acdc15b9030267c6cca743
hollaze <deperiers.a@gmail.com>
2021-04-20 16:24:45 +0200

    checking

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * main - print first 50 fibonnacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int count = 50, i;
+	long int fNum0 = 0, fNum1 = 1;
+	long int fSum = 0, long int limit = 4000000;
+
+	for (i = 0; fsum <= limit; i++)
+	{
+		fSum = fNum0 + fNum1;
+		fNum0 = fNum1;
+		fNum1 = fSum;
+
+		if (fsum % 2 == 0)
+			printf("%li", fSum);
+
+		if (i < 49)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+return (0);
+
+}
