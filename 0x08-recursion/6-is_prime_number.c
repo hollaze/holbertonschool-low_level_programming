6f9a237da96bb6dcbd0ed9660b638c276f83fa3e
hollaze <deperiers.a@gmail.com>
2021-04-22 16:29:02 +0200

    checking

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,28 @@
 #include "holberton.h"
 
 /**
+ * check_prime - check if n is prime
+ * @n: number
+ * @prime: check if n is prime
+ * Return: check_prime
+ */
+
+int check_prime(int n, int prime)
+{
+	if (prime <= 1)
+		return (0);
+
+	/* ex: 17 -> 17 - 1 -> prime number */
+	else if (prime == 2)
+		return (1);
+
+	else if (n % prime == 0)
+		return (0);
+
+	return (check_prime(n, prime - 1));
+}
+
+/**
  * is_prime_number - find prime number
  * @n: number
  *
@@ -9,16 +31,5 @@
 
 int is_prime_number(int n)
 {
-	return(check_prime(n, prime + 1));
-}
-
-int check_prime(int n, int prime)
-{
-
-	if (n % 2 == 0)
-		return (1);
-
-	else
-		return (0);
-
+	return (check_prime(n, n - 1));
 }
