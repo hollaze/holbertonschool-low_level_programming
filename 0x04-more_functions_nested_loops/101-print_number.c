9a1b5bd9a1923592d47796abc18f291d524ef4f0
hollaze <deperiers.a@gmail.com>
2021-04-22 15:20:45 +0200

    checking

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+void print_number(int n)
+{
+	int pow = 1;
+	unsigned int nb;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		nb = -n;
+		count += 1;
+	}
+
+	else
+		nb = n;
+
+	while (nb / pow > 9)
+		pow *= 10;
+
+	while (pow != 0)
+	{
+		count += _putchar((nb / pow) + '0');
+
+		nb %= pow;
+		pow /= 10;
+	}
+}
