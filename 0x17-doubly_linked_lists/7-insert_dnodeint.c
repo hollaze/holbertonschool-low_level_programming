9fe9658e5290c33cfefadc9029d48c07857aca18
hollaze <deperiers.a@gmail.com>
2021-05-02 20:09:53 +0000

    none

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,24 +2,34 @@
 
 /**
  * get_dnodeint_at_index - go to the chosen node of a list
- * @head: head node
- * @index: index of the node starting 0
+ * @h: head node
+ * @idx: index of the node starting 0
+ * @n: data
  * Return: nth node or NULL if node doesn't exist
  */
 
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new;
+
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (*h == NULL || new == NULL)
 		return (NULL);
 
-	while (head != NULL && i < index)
+	while (*h != NULL && i < idx)
 	{
-		head = head->next;
+		*h = (*h)->next;
 		i++;
 	}
 
-return (head);
+	add_dnodeint(h, n);
+
+return (new);
 
 }
