5ee1e62dc063b76f3f3c2afaceb70c1c4f5e8df9
hollaze <deperiers.a@gmail.com>
2021-05-08 09:45:32 +0200

    t

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - nested loop 00 to 99
- *
- *
+ * main -print all possible combinations of double single-digits numbers
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	int numbers0, numbers1;
 
-int numbers0, numbers1;
-
-for (numbers0 = 0; numbers0 <= 9; numbers0++)
-{
-
-for (numbers1 = 0; numbers1 <= 9; numbers1++)
-{
-	putchar(numbers0 + '0');
-	putchar(numbers1 + '0');
-
-
-	if (numbers0 != 9 || numbers1 != 9)
+	for (numbers0 = 0; numbers0 <= 9; numbers0++)
 	{
-	putchar(',');
-	putchar(' ');
+		for (numbers1 = 0; numbers1 <= 9; numbers1++)
+		{
+			putchar(numbers0 + '0');
+			putchar(numbers1 + '0');
+
+			if (numbers0 != 9 || numbers1 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-}
-
-}
-
 	putchar('\n');
 
 return (0);
