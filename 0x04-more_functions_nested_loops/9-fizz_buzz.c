725bd73219b31b6c82cbc5f89e93a0459e69f9d9
hollaze <deperiers.a@gmail.com>
2021-05-08 11:49:02 +0200

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,32 +2,27 @@
 
 /**
  * main - print numbers from 1 to 100. Fizz multiple by 3. Buzz multiple by 5.
+ * FizzBuzz if both
  * Return: Always 0
  */
 
 int main(void)
 {
-int i;
-
+	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-
 		if (i % 3 == 0 && i % 5 == 0)
 		printf("FizzBuzz");
-
 
 		else if (i % 3 == 0)
 		printf("Fizz");
 
-
 		else if (i % 5 == 0)
 		printf("Buzz");
 
-
 		else
 		printf("%u", i);
-
 
 		if (i < 100)
 		putchar(' ');
@@ -35,7 +30,5 @@ int i;
 		else
 		putchar('\n');
 	}
-
 return (0);
-
 }
