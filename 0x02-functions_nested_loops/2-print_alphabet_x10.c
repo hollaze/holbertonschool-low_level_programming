76f21f689b601b1b37ebcfcc5471fe1ffb961d5a
hollaze <deperiers.a@gmail.com>
2021-05-08 10:16:15 +0200

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "holberton.h"
 
 /**
- * print_alphabet_x10 - Prints a to z in lowercase ten times
+ * print_alphabet_x10 - Prints alphabet in lowercase 10 times
  */
 
 void print_alphabet_x10(void)
 {
-int loopX10;
-char c;
+    int loopX10;
+    char c;
 
-for (loopX10 = 0; loopX10 <= 9; loopX10++)
-{
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
-}
+    for (loopX10 = 0; loopX10 <= 9; loopX10++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+            _putchar(c);
+        _putchar('\n');
+    }
 
 }
