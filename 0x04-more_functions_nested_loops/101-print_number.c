ceb9cd0ad79a902ca0be494962e20f597eb45e53
hollaze <deperiers.a@gmail.com>
2021-05-08 11:56:45 +0200

    Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_number - print any number without using printf
- * @n: number used
+ * print_number - print an integer using _putchar
+ * @n: number to print
  */
 
 void print_number(int n)
