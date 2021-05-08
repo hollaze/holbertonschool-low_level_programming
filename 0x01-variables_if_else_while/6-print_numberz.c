64f9bfcdb487c329898feba7c9e94a7efd653d40
hollaze <deperiers.a@gmail.com>
2021-05-08 09:27:23 +0200

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - print 0 to 10 without using char
+ * main - print all digits of base 10 starting 0 
+ * without using char
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+    int base = 0;
 
-int base = 0;
-
-while (base <= 9)
-{
-putchar (base + '0');
-base++;
-}
-
-putchar ('\n');
+    while (base <= 9)
+    {
+        putchar (base + '0');
+        base++;
+    }
+    putchar ('\n');
 
 return (0);
 }
