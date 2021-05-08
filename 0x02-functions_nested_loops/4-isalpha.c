c1d4012d9eec9500676a4bebdb92bed709e94b3b
hollaze <deperiers.a@gmail.com>
2021-05-08 10:18:45 +0200

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 
 /**
- * _isalpha - check if c is character
- * @c: character to be check
+ * _isalpha - check if c is an alphabetic character
+ * @c: character to check
  *
  * Return: 1 if c isalpha. 0 otherwise
  */
 
 int _isalpha(int c)
 {
-if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
-return (1);
+    if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+        return (1);
 
-else
-return (0);
+    return (0);
 }
