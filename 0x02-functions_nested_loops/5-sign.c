8b7080eacbc94185542db1cabd185195244b520e
hollaze <deperiers.a@gmail.com>
2021-05-08 10:21:30 +0200

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,30 +2,25 @@
 
 /**
  * print_sign - print the sign of a number
- * @n: the number to be check
+ * @n: the number to check
  *
- * Return: -1 if n is less than 0 -- 0 if n is 0 -- 1 if n is greater than 0
+ * Return: -1 if n is less than 0; 0 if n is 0; 1 if n is greater than 0
  */
 
 int print_sign(int n)
 {
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
 
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
+     else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
 
-else if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-
-else
-{
-_putchar('-');
-return (-1);
-}
-
+    _putchar('-');
+    return (-1);
 }
