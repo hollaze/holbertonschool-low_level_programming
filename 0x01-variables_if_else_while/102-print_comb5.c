b322179a6902e323f2a8c614b28cb7ac6ba32c51
hollaze <deperiers.a@gmail.com>
2021-05-08 09:54:13 +0200

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,41 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - Nested loops - printing 00 01 to 98 99
- *
- *
+ * main - prints all possible combinations of two two-digit numbers
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	int numbers0, numbers1;
 
-int numbers0, numbers1;
-
-for (numbers0 = 0; numbers0 <= 98; numbers0++)
-{
-for (numbers1 = numbers0 + 1; numbers1 <= 99; numbers1++)
-{
-
-	putchar(numbers0 / 10 + '0');
-	putchar(numbers0 % 10 + '0');
-	putchar(' ');
-	putchar(numbers1 / 10 + '0');
-	putchar(numbers1 % 10 + '0');
-
-
-	if (numbers0 != 98 || numbers1 != 99)
+	for (numbers0 = 0; numbers0 <= 98; numbers0++)
 	{
-	putchar(',');
-	putchar(' ');
+		for (numbers1 = numbers0 + 1; numbers1 <= 99; numbers1++)
+		{
+			putchar(numbers0 / 10 + '0');
+			putchar(numbers0 % 10 + '0');
+			putchar(' ');
+			putchar(numbers1 / 10 + '0');
+			putchar(numbers1 % 10 + '0');
+
+			if (numbers0 != 98 || numbers1 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-
-
-
-} /* For numbers1 */
-} /* For number0 */
-
 	putchar('\n');
 
 return (0);
