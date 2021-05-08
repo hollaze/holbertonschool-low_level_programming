47de2c09edd36a1620810010cec1cff94c0e8fd9
hollaze <deperiers.a@gmail.com>
2021-05-08 10:41:34 +0200

    Holberton School, 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - Print numbers from 0 to 98
+ * print_to_98 - prints all natural numbers from n to 98
  * @n: int
  */
 
 void print_to_98(int n)
 {
-
 	while (n < 98)
 	{
-
 		printf("%d", n);
 
 		putchar(',');
@@ -21,17 +19,12 @@ void print_to_98(int n)
 
 	for (; n > 98; n--)
 	{
-
 		printf("%d", n);
 
 		putchar(',');
 		putchar(' ');
-
 	}
 
 	if (n == 98)
-		printf("%d", n);
-
-	putchar('\n');
-
+		printf("%d\n", n);
 }
