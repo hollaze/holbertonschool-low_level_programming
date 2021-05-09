df7f0e3953ec7aeef2bbf36f59f1a025e72e0a8b
hollaze <deperiers.a@gmail.com>
2021-05-09 15:30:01 +0200

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _puts - ptrins string ,followed by new line ,to stdout
+ * _puts - prints a string
  * @str: char
  */
 
@@ -10,10 +10,6 @@ void _puts(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		_putchar(str[i]);
-	}
-
 	_putchar('\n');
-
 }
