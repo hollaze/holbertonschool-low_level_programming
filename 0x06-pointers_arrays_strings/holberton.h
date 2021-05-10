5678ab9683140f1f278152acbcb4b3eaef39de86
hollaze <deperiers.a@gmail.com>
2021-05-10 05:12:21 +0200

    Function prototypes

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int _puchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -8,3 +14,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+
+#endif /* HOLBERTON_H */
