12cc4e1698a5dad2dadaef86f2922b66a8e61e42
hollaze <deperiers.a@gmail.com>
2021-05-10 06:08:27 +0200

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,15 +10,13 @@
 
 char *string_toupper(char *s)
 {
-
 	int i;
 
-	for (i = 0; c[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
 	}
 
 return (s);
-
 }
