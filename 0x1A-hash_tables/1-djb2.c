1ecaeca2ce3461f3de532726392f63d889c2698b
hollaze <deperiers.a@gmail.com>
2021-05-30 14:08:00 +0200

    Checking

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,3 @@
-#include "hash_tables.h"
-
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
@@ -14,7 +12,8 @@ unsigned long int hash_djb2(const unsigned int *str)
 
 	hash = 5381;
 	while ((c = *str++))
+	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-
+	}
 	return (hash);
 }
