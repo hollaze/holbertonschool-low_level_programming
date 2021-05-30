6c823309e5d209aa4eababf6f0fa266d155d373a
hollaze <deperiers.a@gmail.com>
2021-05-30 13:18:15 +0200

    Adding description + NULL -> (NULL)

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - creating a simple hash table
+ * @size: size of hash table
+ * Return: pointer to newly created hash table
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
@@ -7,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_hash_table = malloc(sizeof(hash_table_t));
 
 	if (new_hash_table == NULL)
-		return NULL;
+		return (NULL);
 
 	new_hash_table->size = size;
 
