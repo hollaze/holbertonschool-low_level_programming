2e3e22f275ea23cbae450469911a76cadb850f20
hollaze <deperiers.a@gmail.com>
2021-05-30 13:15:59 +0200

    Checking

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
+
+	if (new_hash_table == NULL)
+		return NULL;
+
+	new_hash_table->size = size;
+
+	return (new_hash_table);
+}
