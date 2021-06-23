71b583aba6b3a63154d62e2d8ba7b1c9fe47e02f
hollaze <deperiers.a@gmail.com>
2021-06-23 18:10:17 +0200

    ugly but working

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,30 +17,27 @@ char *_strstr(char *haystack, char *needle)
 	while (needle[len] != '\0')
 		len++;
 
-	/*finding needle in haystack -> for loop comparison -> nested loops (2) -> compare elements of haystack & elements of needle*/
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		while (needle[j] == haystack[i])
 		{
 			j++;
 		}
-		/*if they all good -> break*/
 		if (j == len)
 			break;
 	}
 
-	/*if found -> index of haystack - len of needle  -> gives pointer to beginning of located string*/
-
 	if (j != len)
 		return (NULL);
 
-	/*haystack have to increment to its index that have been substrated by len of needle -> while (*haystack < idx) -> haystack++;*/
 	j = 0;
+	if (len > 0)
+		i -= len - 1;
+
 	while (j < i)
 	{
 		j++;
 		haystack++;
 	}
-	/*return pointer to beginning of located substring -> pointer to haystack -> or NULL if not found*/
 	return (haystack);
 }
