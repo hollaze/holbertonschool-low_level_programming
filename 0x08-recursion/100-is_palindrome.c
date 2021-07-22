3ce949749d5721556c04e5c1117ffa1db8a8792f
hollaze <deperiers.a@gmail.com>
2021-07-22 19:30:02 +0200

    string is a palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
+
+/**
+ * _strlen - count lenght of string
+ * @s: string
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen(s + 1) + 1);
+}
 
 /**
  * check_palindrome - check if string is a palindrome
@@ -29,13 +41,8 @@ int check_palindrome(char *s, int start, int end)
 
 int is_palindrome(char *s)
 {
-	int len = 0;
-
 	if (*s == '\0')
 		return (1);
 
-	while (s[len] != '\0')
-		len++;
-
-	return (check_palindrome(s, 0, len - 1));
+	return (check_palindrome(s, 0, _strlen(s) - 1));
 }
