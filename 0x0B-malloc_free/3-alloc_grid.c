108a930b0bf4eac4c70dc7cd2d58d808236d6cb5
hollaze <deperiers.a@gmail.com>
2021-07-27 14:04:24 +0200

    l24: change in malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(sizeof(int) * height);
+		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < width; j++)
